b3be832a0e841eafb2c874989923f9580c748589
Stephen Hila <stephen.hila@gmail.com>
2018-06-05 07:07:18 +0800

    - Continued CombinationTypeEvaluator. - Continued CombinationTypeEvaluatorUnitTest.

--- TebbyPoker.GameEngine.UnitTest/CombinationTypeEvaluatorUnitTest.cs
+++ TebbyPoker.GameEngine.UnitTest/CombinationTypeEvaluatorUnitTest.cs
@@ -1,15 +1,220 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TebbyPoker.GameEngine.Contracts;
+using TebbyPoker.Models;
 
 namespace TebbyPoker.GameEngine.UnitTest
 {
     [TestClass]
     public class CombinationTypeEvaluatorUnitTest
     {
-        [TestMethod]
-        public void TestMethod1()
+        ICombinationTypeEvaluator unitUnderTest;
+
+        [TestInitialize]
+        public void TestInitialize()
         {
-            throw new NotImplementedException();
+            unitUnderTest = new CombinationTypeEvaluator();
+        }
+
+        [TestMethod]
+        public void Evaluate_ReturnsCorrectCombinationType_RoyalFlush()
+        {
+            // Arrange
+            List<Card> cards = new List<Card>();
+            cards.Add(new Card(Rank.Ace, Suit.Spade));
+            cards.Add(new Card(Rank.King, Suit.Spade));
+            cards.Add(new Card(Rank.Queen, Suit.Spade));
+            cards.Add(new Card(Rank.Jack, Suit.Spade));
+            cards.Add(new Card(Rank.Ten, Suit.Spade));
+            cards.Add(new Card(Rank.Nine, Suit.Spade));
+            cards.Add(new Card(Rank.Seven, Suit.Heart));
+
+            // Act
+            Combination combination = unitUnderTest.Evaluate(cards);
+
+            // Assert
+            Assert.AreEqual(combination, Combination.RoyalFlush);
+        }
+
+        [TestMethod]
+        public void Evaluate_ReturnsCorrectCombinationType_StraightFlush()
+        {
+            // Arrange
+            List<Card> cards = new List<Card>();
+            cards.Add(new Card(Rank.Ace, Suit.Spade));
+            cards.Add(new Card(Rank.Two, Suit.Spade));
+            cards.Add(new Card(Rank.Three, Suit.Spade));
+            cards.Add(new Card(Rank.Four, Suit.Spade));
+            cards.Add(new Card(Rank.Five, Suit.Spade));
+            cards.Add(new Card(Rank.Six, Suit.Heart));
+            cards.Add(new Card(Rank.Seven, Suit.Heart));
+
+            // Act
+            Combination combination = unitUnderTest.Evaluate(cards);
+
+            // Assert
+            Assert.AreEqual(combination, Combination.StraightFlush);
+        }
+
+        [TestMethod]
+        public void Evaluate_ReturnsCorrectCombinationType_FourOfAKind()
+        {
+            // Arrange
+            List<Card> cards = new List<Card>();
+            cards.Add(new Card(Rank.Ace, Suit.Spade));
+            cards.Add(new Card(Rank.Ace, Suit.Heart));
+            cards.Add(new Card(Rank.Ace, Suit.Diamond));
+            cards.Add(new Card(Rank.Ace, Suit.Club));
+            cards.Add(new Card(Rank.Queen, Suit.Spade));
+            cards.Add(new Card(Rank.Queen, Suit.Heart));
+            cards.Add(new Card(Rank.Queen, Suit.Diamond));
+
+            // Act
+            Combination combination = unitUnderTest.Evaluate(cards);
+
+            // Assert
+            Assert.AreEqual(combination, Combination.FourOfAKind);
+        }
+
+        [TestMethod]
+        public void Evaluate_ReturnsCorrectCombinationType_FullHouse()
+        {
+            // Arrange
+            List<Card> cards = new List<Card>();
+            cards.Add(new Card(Rank.Ace, Suit.Spade));
+            cards.Add(new Card(Rank.Ace, Suit.Heart));
+            cards.Add(new Card(Rank.Ace, Suit.Diamond));
+            cards.Add(new Card(Rank.King, Suit.Spade));
+            cards.Add(new Card(Rank.King, Suit.Heart));
+            cards.Add(new Card(Rank.King, Suit.Diamond));
+            cards.Add(new Card(Rank.Queen, Suit.Spade));
+
+            // Act
+            Combination combination = unitUnderTest.Evaluate(cards);
+
+            // Assert
+            Assert.AreEqual(combination, Combination.FullHouse);
+        }
+
+        [TestMethod]
+        public void Evaluate_ReturnsCorrectCombinationType_Flush()
+        {
+            // Arrange
+            List<Card> cards = new List<Card>();
+            cards.Add(new Card(Rank.Ace, Suit.Spade));
+            cards.Add(new Card(Rank.Three, Suit.Spade));
+            cards.Add(new Card(Rank.Five, Suit.Spade));
+            cards.Add(new Card(Rank.Seven, Suit.Spade));
+            cards.Add(new Card(Rank.Nine, Suit.Spade));
+            cards.Add(new Card(Rank.Nine, Suit.Heart));
+            cards.Add(new Card(Rank.Seven, Suit.Heart));
+
+            // Act
+            Combination combination = unitUnderTest.Evaluate(cards);
+
+            // Assert
+            Assert.AreEqual(combination, Combination.Flush);
+        }
+
+        [TestMethod]
+        public void Evaluate_ReturnsCorrectCombinationType_Straight()
+        {
+            // Arrange
+            List<Card> cards = new List<Card>();
+            cards.Add(new Card(Rank.Ace, Suit.Spade));
+            cards.Add(new Card(Rank.Two, Suit.Diamond));
+            cards.Add(new Card(Rank.Three, Suit.Heart));
+            cards.Add(new Card(Rank.Four, Suit.Heart));
+            cards.Add(new Card(Rank.Five, Suit.Spade));
+            cards.Add(new Card(Rank.Seven, Suit.Heart));
+            cards.Add(new Card(Rank.King, Suit.Heart));
+
+            // Act
+            Combination combination = unitUnderTest.Evaluate(cards);
+
+            // Assert
+            Assert.AreEqual(combination, Combination.Straight);
+        }
+
+        [TestMethod]
+        public void Evaluate_ReturnsCorrectCombinationType_ThreeOfAKind()
+        {
+            // Arrange
+            List<Card> cards = new List<Card>();
+            cards.Add(new Card(Rank.Six, Suit.Spade));
+            cards.Add(new Card(Rank.Six, Suit.Club));
+            cards.Add(new Card(Rank.Six, Suit.Diamond));
+            cards.Add(new Card(Rank.Ace, Suit.Spade));
+            cards.Add(new Card(Rank.Five, Suit.Club));
+            cards.Add(new Card(Rank.Seven, Suit.Spade));
+            cards.Add(new Card(Rank.King, Suit.Club));
+
+            // Act
+            Combination combination = unitUnderTest.Evaluate(cards);
+
+            // Assert
+            Assert.AreEqual(combination, Combination.ThreeOfAKind);
+        }
+
+        [TestMethod]
+        public void Evaluate_ReturnsCorrectCombinationType_TwoPair()
+        {
+            // Arrange
+            List<Card> cards = new List<Card>();
+            cards.Add(new Card(Rank.Jack, Suit.Spade));
+            cards.Add(new Card(Rank.Jack, Suit.Diamond));
+            cards.Add(new Card(Rank.Five, Suit.Spade));
+            cards.Add(new Card(Rank.Five, Suit.Diamond));
+            cards.Add(new Card(Rank.Seven, Suit.Club));
+            cards.Add(new Card(Rank.Eight, Suit.Spade));
+            cards.Add(new Card(Rank.Nine, Suit.Club));
+
+            // Act
+            Combination combination = unitUnderTest.Evaluate(cards);
+
+            // Assert
+            Assert.AreEqual(combination, Combination.TwoPair);
+        }
+
+        [TestMethod]
+        public void Evaluate_ReturnsCorrectCombinationType_OnePair()
+        {
+            // Arrange
+            List<Card> cards = new List<Card>();
+            cards.Add(new Card(Rank.Queen, Suit.Spade));
+            cards.Add(new Card(Rank.Queen, Suit.Diamond));
+            cards.Add(new Card(Rank.Four, Suit.Spade));
+            cards.Add(new Card(Rank.Two, Suit.Diamond));
+            cards.Add(new Card(Rank.Seven, Suit.Club));
+            cards.Add(new Card(Rank.Eight, Suit.Spade));
+            cards.Add(new Card(Rank.Nine, Suit.Club));
+
+            // Act
+            Combination combination = unitUnderTest.Evaluate(cards);
+
+            // Assert
+            Assert.AreEqual(combination, Combination.OnePair);
+        }
+
+        [TestMethod]
+        public void Evaluate_ReturnsCorrectCombinationType_HighCard()
+        {
+            // Arrange
+            List<Card> cards = new List<Card>();
+            cards.Add(new Card(Rank.Ace, Suit.Spade));
+            cards.Add(new Card(Rank.Queen, Suit.Diamond));
+            cards.Add(new Card(Rank.Four, Suit.Spade));
+            cards.Add(new Card(Rank.Two, Suit.Diamond));
+            cards.Add(new Card(Rank.Seven, Suit.Club));
+            cards.Add(new Card(Rank.Eight, Suit.Spade));
+            cards.Add(new Card(Rank.Nine, Suit.Club));
+
+            // Act
+            Combination combination = unitUnderTest.Evaluate(cards);
+
+            // Assert
+            Assert.AreEqual(combination, Combination.HighCard);
         }
     }
 }
--- TebbyPoker.GameEngine/CombinationTypeEvaluator.cs
+++ TebbyPoker.GameEngine/CombinationTypeEvaluator.cs
@@ -54,10 +54,16 @@ namespace TebbyPoker.GameEngine
             }
         }
 
-#warning TODO: try and think of a pattern to use for each combination-related methods which return bool.
         public bool IsRoyalFlush(List<Card> cards)
         {
-            throw new NotImplementedException();
+            foreach (var ace in cards.Where(c => c.Rank == Rank.Ace))
+            {
+                return cards.Exists(c => c.Suit == ace.Suit && c.Rank == Rank.King)
+                    && cards.Exists(c => c.Suit == ace.Suit && c.Rank == Rank.Queen)
+                    && cards.Exists(c => c.Suit == ace.Suit && c.Rank == Rank.Jack)
+                    && cards.Exists(c => c.Suit == ace.Suit && c.Rank == Rank.Ten);
+            }
+            return false;
         }
 
         public bool IsStraightFlush(List<Card> cards)
@@ -67,12 +73,32 @@ namespace TebbyPoker.GameEngine
 
         public bool IsFourOfAKind(List<Card> cards)
         {
-            throw new NotImplementedException();
+            foreach (var cardGroup in cards.GroupBy(c => c.Rank))
+            {
+                if (cardGroup.Count() == 4)
+                {
+                    return true;
+                }
+            }
+            return false;
         }
 
         public bool IsFullHouse(List<Card> cards)
         {
-            throw new NotImplementedException();
+            bool hasThreeOfAKind = false;
+            bool hasAPair = false;
+            foreach (var cardGroup in cards.GroupBy(c => c.Rank))
+            {
+                if (cardGroup.Count() == 3)
+                {
+                    hasThreeOfAKind = true;
+                }
+                else if (cardGroup.Count() == 2)
+                {
+                    hasAPair = true;
+                }
+            }
+            return hasThreeOfAKind && hasAPair;
         }
 
         public bool IsFlush(List<Card> cards)
@@ -94,17 +120,42 @@ namespace TebbyPoker.GameEngine
 
         public bool IsThreeOfAKind(List<Card> cards)
         {
-            throw new NotImplementedException();
+            foreach (var cardGroup in cards.GroupBy(c => c.Rank))
+            {
+                if (cardGroup.Count() == 3)
+                {
+                    return true;
+                }
+            }
+            return false;
         }
 
         public bool IsTwoPair(List<Card> cards)
         {
-            throw new NotImplementedException();
+            int groupCount = 0;
+            foreach (var cardGroup in cards.GroupBy(c => c.Rank))
+            {
+                if (cardGroup.Count() == 2)
+                {
+                    groupCount++;
+                }
+            }
+
+            return groupCount > 1;
         }
 
         public bool IsOnePair(List<Card> cards)
         {
-            throw new NotImplementedException();
+            int groupCount = 0;
+            foreach (var cardGroup in cards.GroupBy(c => c.Rank))
+            {
+                if (cardGroup.Count() == 2)
+                {
+                    groupCount++;
+                }
+            }
+
+            return groupCount == 1;
         }
     }
 }
