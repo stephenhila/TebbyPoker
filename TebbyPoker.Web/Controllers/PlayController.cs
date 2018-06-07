fb32ff090470804d34e124b5ee1c50c2a797517a
Stephen Hila <stephen.hila@gmail.com>
2018-06-07 08:46:09 +0800

    - Added Game page to handle game flow, pending completion. - COntinued implementation.

--- TebbyPoker.Web/Controllers/PlayController.cs
+++ TebbyPoker.Web/Controllers/PlayController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 using TebbyPoker.GameEngine;
 using TebbyPoker.GameEngine.Contracts;
 using TebbyPoker.Managers;
+using TebbyPoker.Models;
 using TebbyPoker.Web.Models;
 
 namespace TebbyPoker.Web.Controllers
@@ -40,6 +41,21 @@ namespace TebbyPoker.Web.Controllers
         {
             model.Players.Remove(player);
             return Index(model);
+        }
+
+        [HttpPost]
+        public ActionResult StartGame(PlayIndexViewModel model)
+        {
+            List<Player> players = new List<Player>();
+
+            foreach (var playerName in model.Players)
+            {
+                players.Add(new Player(playerName));
+            }
+
+            PlayGameViewModel gameModel = new PlayGameViewModel(players);
+
+            return View("Game", gameModel);
         }
     }
 }
---
+++ TebbyPoker.Web/Models/PlayGameViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TebbyPoker.Managers;
+using TebbyPoker.Models;
+
+namespace TebbyPoker.Web.Models
+{
+    public class PlayGameViewModel
+    {
+        List<Player> _players;
+        public IEnumerable<Player> Players
+        {
+            get { return _players; }
+        }
+
+        Deck _deck;
+        public Deck Deck { get { return _deck; } }
+
+        public PlayGameViewModel(List<Player> players)
+        {
+            _players = players;
+
+            _deck = new Deck();
+            _deck.Shuffle(3);
+
+            DistributeCards(_players, _deck);
+        }
+
+        void DistributeCards(List<Player> players, Deck deck)
+        {
+            foreach (var player in players)
+            {
+                player.GetCard(deck);
+            }
+        }
+    }
+}
